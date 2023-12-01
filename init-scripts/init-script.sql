e3379d9e7b38e221428f95c9f8f521e376abee0f
jato ulrich guiffo kengne <ulrichjato@yahoo.fr>
2023-12-01 01:15:37 -0500

    Updated Jenkins File

--- init-scripts/init-script.sql
+++ init-scripts/init-script.sql
@@ -1,6 +1,7 @@
-CREATE TABLE roles (
+USE certificatetracker;
+CREATE TABLE certificatetracker.roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(20) NOT NULL
 );
 
-INSERT INTO roles (name) VALUES ("ROLE_USER"),("ROLE_MODERATOR"), ("ROLE_ADMIN");
+INSERT INTO roles (name) VALUES ('ROLE_USER'),('ROLE_MODERATOR'), ('ROLE_ADMIN');
